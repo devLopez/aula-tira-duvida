cc3e8c0f18b6b7105045d0138d5f12d6b71c9c80
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2018-03-08 21:06:20 -0300

    Criacao da estrutura do banco de dados

---
+++ banco-de-dados.sql
@@ -0,0 +1,12 @@
+-- Aqui realizamos a criação do banco de dados;
+create database mensagens;
+
+-- Aqui setamos qual banco devemos utilizar
+use mensagens;
+
+-- aqui realizamos a criação da tabela mensagens
+create database mensagens(
+    id int not null primary key AUTO_INCREMENT,
+    nome varchar(46) not null,
+    mensagem varchar(250) not null
+);
